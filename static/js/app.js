a667737ea4ac76d481a680a059801a6fa31aafa9
James <jamess2eagle@gmail.com>
2020-09-19 18:36:27 -0700

    rough draft

--- static/js/app.js
+++ static/js/app.js
@@ -22,7 +22,7 @@ function plotid(id) {
             .text(row);
         });
         
-        //setting and selecting top 10 values
+        //filtering and sorting top 10 values
         var sample = filteredData[0].samples.filter(row => row.id === id);
         var otu_ids = sample.map(row => row.otu_ids)[0].slice(0,10).reverse();
         var otu_no = otu_ids.map(count => "OTU " + count);
@@ -50,6 +50,7 @@ function plotid(id) {
         //plotting bar chart
         Plotly.newPlot("bar", data, layout);
 
+        //trace for bubble plot
         var trace2 = {
             x: otu_ids = sample.map(row => row.otu_ids)[0],
             y: sample.map(row => row.sample_values)[0],
@@ -63,20 +64,22 @@ function plotid(id) {
 
         data2 = [trace2];
 
+        //layout for bubble plot
         var layout2 = {
             xaxis:{title: "OTU ID"},
             title: "volume by OTU ID",
             height: 700,
             width: 1170
         };
-
+        
+        //plotting bubble
         Plotly.newPlot("bubble", data2, layout2);
 
-
+        //filtering demographic info
         var sample2 = filteredData[0].metadata.filter(row => row.id.toString() === id)[0];
         var demo = d3.select("#sample-metadata");
+        //remove & append demographic info
         demo.selectAll("div").remove();
-        console.log(sample2);
         demo.append("div").text(`id: ${sample2.id}`);
         demo.append("div").text(`ethinicity: ${sample2.ethinicity}`);
         demo.append("div").text(`gender: ${sample2.gender}`);
@@ -84,6 +87,41 @@ function plotid(id) {
         demo.append("div").text(`location: ${sample2.location}`);
         demo.append("div").text(`bbtype: ${sample2.bbtype}`);
         demo.append("div").text(`wfreq: ${sample2.wfreq}`);
+        
+        //filtering gauge plot
+        var frequency = filteredData[0].metadata.filter(row => row.id.toString() === id)[0].wfreq;
+        
+        //trace for gauge plot
+        var trace3 = [
+            {
+            domain: { x: [0, 1], y: [0, 1] },
+            value: frequency,
+            title: { text: "Weekly Washing Frequency"},
+            type: "indicator",
+            
+            mode: "gauge+number",
+            gauge: { axis: { range: [null, 9] },
+                     steps: [
+                      { range: [0, 1], color: "#ffffe5"},
+                      { range: [1, 2], color: "#fdfeda"},
+                      { range: [2, 3], color: "#fbfdcf"},
+                      { range: [3, 4], color: "#f7fcc5"},
+                      { range: [4, 5], color: "#f3fabd"},
+                      { range: [5, 6], color: "#eef8b5"},
+                      { range: [6, 7], color: "#e7f6ae"},
+                      { range: [7, 8], color: "#dff2a8"},
+                      { range: [8, 9], color: "#d6efa3"},
+                    ]} 
+            }
+          ];
+        
+        //gauge layout
+          var layout3 = { 
+              margin: { t: 20, b: 40, l:100, r:100 } 
+            };
+        //plotting gauge plot
+          Plotly.newPlot("gauge", trace3, layout3);
+        
 
     });
 };
